720649e7bc60228b085b42a95af5b4d95d6e5d34
nyximos <nyximos@gmail.com>
2022-02-07 14:14:28 +0900

    Initial commit

---
+++ src/main/java/wd/team4/everycare/EverycareApplication.java
@@ -0,0 +1,13 @@
+package wd.team4.everycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverycareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverycareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wd/team4/everycare/EverycareApplicationTests.java
@@ -0,0 +1,13 @@
+package wd.team4.everycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverycareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
