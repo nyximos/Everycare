bccbee7000a2a1660e2073111c2a33a4da57b672
nyximos <nyximos@gmail.com>
2022-03-08 20:49:31 +0900

    Create CareTarget.java

---
+++ src/main/java/wd/team4/everycare/domain/CareTarget.java
@@ -0,0 +1,70 @@
+package wd.team4.everycare.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "care_target")
+@SequenceGenerator(name = "care_target_seq_generator",
+        sequenceName = "care_target_seq",
+        initialValue = 1, allocationSize = 1)
+public class CareTarget {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "care_target_seq_generator")
+    @Column(name = "care_target_id", nullable = false)
+    private Long id;
+
+    @Column(name = "care_target_name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "care_target_gender", nullable = false)
+    private String gender; // enum
+
+    @Column(name = "care_target_birth", nullable = false)
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private LocalDate birth;
+
+    @Column(name = "care_target_height", nullable = false)
+    private Long height;
+
+    @Column(name = "care_target_weight", nullable = false)
+    private Long weight;
+
+    @Column(name = "care_target_zipcode", length = 45, nullable = false)
+    private String zipcode;
+
+    @Column(name = "care_target_address", length = 255, nullable = false)
+    private String address;
+
+    @Column(name = "care_target_detailed_address", length = 255, nullable = false)
+    private String detailedAddress;
+
+    @Column(name = "care_target_long_term_care_grade", nullable = false)
+    private int longTermCareGrade;
+
+    @Column(name = "care_target_comment", length = 500)
+    private String comment;
+
+    @Column(name = "care_target_pet", nullable = false)
+    private String pet; // enum
+
+    @Column(name = "care_target_is_cctv_agrement", nullable = false)
+    private String isCctvAgrement; // enum
+
+    @Column(name = "care_target_care_tjype", length = 50, nullable = false)
+    private String careType; // enum
+
+    @Column(name = "care_target_corona_test", nullable = false)
+    private String coronaTest; // enum
+
+}
