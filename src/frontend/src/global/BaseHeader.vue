ecc9e13982c1f89b4a86dbe068c1d90719136aac
nyximos <nyximos@gmail.com>
2022-03-28 11:02:33 +0900

    Update BaseHeader.vue

--- src/frontend/src/global/BaseHeader.vue
+++ src/frontend/src/global/BaseHeader.vue
@@ -1,17 +1,48 @@
 <template>
-<div class="navbar">
-    <v-app-bar max-height="50" color="blue">
-      
-    </v-app-bar>
-    
-</div>    
+    <div class="header">
+        <div class="logo" @click="goMain()">logo</div>
+        <div class="list">
+            <v-btn text>케어매칭</v-btn>
+            <v-btn text>케어노트</v-btn>
+            <v-btn text>케어스토어</v-btn>
+        </div>
+        <div>
+            <v-btn text>로그인</v-btn>
+            <v-btn text>회원가입</v-btn>
+        </div>
+    </div>
 </template>
+
 <script>
-export default {};
-</script>
-<style>
-    .menu{
-        color: black;
-        display: inline;
+export default {
+    methods: {
+        goMain() {
+            if (this.$router.history.current.fullPath == '/') {
+                console.log('go main');
+            } else {
+                this.$router.push('/');
+                console.log('/');
+            }
+        }
     }
+};
+</script>
+<style scoped>
+.header {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    height: 50px;
+    color: black;
+    background: #69f0ae;
+    align-items: center;
+}
+.logo {
+    padding-left: 10px;
+    cursor: pointer;
+}
+.list {
+    display: flex;
+    align-items: center;
+}
 </style>
