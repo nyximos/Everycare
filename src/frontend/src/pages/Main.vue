f681bc50883794e6dc023f6626b55fc3825f107b
nyximos <nyximos@gmail.com>
2022-03-28 11:03:07 +0900

    Update Main.vue

--- src/frontend/src/pages/Main.vue
+++ src/frontend/src/pages/Main.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
+    <v-container fluid class="main">
         <Main />
-    </div>
+        <ul>
+            <li class="menu"><router-link to="/Caresitters">케어매칭</router-link></li>
+            <li><router-link to="/Profile">케어프로필</router-link></li>
+            <li><router-link to="/Careprofile">케어시터프로필 등록</router-link></li>
+            <li><router-link to="/Carepeople">케어대상인 조회</router-link></li>
+            <li><router-link to="/Alarm">알림</router-link></li>
+            <li><router-link to="/Profile2">프로필상세</router-link></li>
+        </ul>
+    </v-container>
 </template>
 <script>
-import Main from '@/components/main.vue';
+import Main from '@/components/main/index';
 export default {
     components: {
         Main
