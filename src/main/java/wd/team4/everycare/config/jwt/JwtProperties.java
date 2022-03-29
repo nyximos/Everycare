991f59bf703c37c14d19279623f5d98ac57d4484
nyximos <nyximos@gmail.com>
2022-03-29 20:24:21 +0900

    파일 업로드

--- src/main/java/wd/team4/everycare/config/jwt/JwtProperties.java
+++ src/main/java/wd/team4/everycare/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package wd.team4.everycare.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "team4"; // 우리 서버만 알고 있는 비밀값
-    int EXPIRATION_TIME = 1000 * 60 * 60; // 1시간
+    int EXPIRATION_TIME = 1000 * 60 * 10; // 10분
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
