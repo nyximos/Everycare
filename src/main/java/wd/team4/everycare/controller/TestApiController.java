d816d486ddbd25188d89b70e01cd7e6457774aad
nyximos <nyximos@gmail.com>
2022-04-04 16:38:30 +0900

    test

--- src/main/java/wd/team4/everycare/controller/TestApiController.java
+++ src/main/java/wd/team4/everycare/controller/TestApiController.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
+public class TestApiController {
 
     @ResponseBody
     @GetMapping("/test")
