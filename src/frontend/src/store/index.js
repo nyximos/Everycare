d7bd976e845343cc835cc09fb2792395128686a4
QQTET <85005784+QQTET@users.noreply.github.com>
2022-04-06 15:32:15 +0900

    Update index.js

--- src/frontend/src/store/index.js
+++ src/frontend/src/store/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import modules from '@/store/modules/index.js';
 
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({ modules });
+export default new Vuex.Store({ 
+  modules
+});
+
