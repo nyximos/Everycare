c05bfebf3c31829d64b4fb58ac7d46f2f6a5ae0a
nyximos <nyximos@gmail.com>
2022-04-11 06:17:28 +0900

    케어 대상인 삭제

--- src/main/java/wd/team4/everycare/controller/CareTargetApiController.java
+++ src/main/java/wd/team4/everycare/controller/CareTargetApiController.java
@@ -7,13 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import wd.team4.everycare.config.auth.PrincipalDetails;
+import wd.team4.everycare.domain.CareTarget;
 import wd.team4.everycare.domain.Member;
 import wd.team4.everycare.dto.CareTargetFormDTO;
 import wd.team4.everycare.dto.response.MyResponse;
 import wd.team4.everycare.dto.response.StatusEnum;
+import wd.team4.everycare.repository.CareTargetRepository;
 import wd.team4.everycare.service.CareTargetServiceImpl;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ import java.io.IOException;
 public class CareTargetApiController {
 
     private final CareTargetServiceImpl careTargetService;
+    private final CareTargetRepository careTargetRepository;
 
     @ResponseBody
     @PostMapping("/carenote/caretargets/new")
@@ -42,5 +46,15 @@ public class CareTargetApiController {
 
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<MyResponse>(body, headers, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/carenote/caretargets/{id}")
+    public ResponseEntity<MyResponse> deleteCareTarget(@PathVariable("id") Long id ){
+        careTargetRepository.deleteById(id);
+        MyResponse body = MyResponse.builder()
+                .header(StatusEnum.OK)
+                .message("성공했슴다~")
+                .build();
+        return new ResponseEntity<MyResponse>(body, HttpStatus.OK);
     }
 }
