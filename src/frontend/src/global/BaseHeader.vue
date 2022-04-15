85eebe29b8c88efc4cb5837ebec5f01bd39da867
QQTET <85005784+QQTET@users.noreply.github.com>
2022-04-15 17:32:13 +0900

    로그인 분기처리 작업중1

--- src/frontend/src/global/BaseHeader.vue
+++ src/frontend/src/global/BaseHeader.vue
@@ -8,9 +8,13 @@
                 <v-btn text>케어스토어</v-btn>
             </div>
             <div>
-                    
+                <div v-if="cookie">
+                    <router-link to="/" @click="logoutUser">로그아웃</router-link>
+                </div>
+                <div v-else>
                     <router-link to="/login">로그인</router-link>
                     <router-link to="/signup">회원가입</router-link>
+                </div>
             </div>
         </div>
     </header>
@@ -18,22 +22,33 @@
 
 <script>
 export default {
-    computed: {
-
-    },
-    methods: {
-        goMain() {
-            if (this.$router.history.current.fullPath == '/') {
-                console.log('go main');
-            } else {
-                this.$router.push('/');
-                console.log('/');
+    data() {
+        return {
+            computed: {
+                cookie(){
+                    return document.cookie
+                }
+            },
+            methods: {
+                goMain() {
+                    if (this.$router.history.current.fullPath == '/') {
+                        console.log('go main');
+                    } else {
+                        this.$router.push('/');
+                        console.log('/');
+                    }
+                },
+                logoutUser() {
+                    const delCookie = function delCookie_by_name(name) {
+                        let date = new Date();
+                        date.setDate(date.getDate() - 100);
+                        let Cookie = `${name}=;Expires=${date.toUTCString()}`;
+                        document.cookie = Cookie;
+                    };
+                    delCookie('Authorization');
+                }
             }
-        },
-        logoutUser(){
-            this.$stroe.commit('userStore2/clearUsername');
-            this.$router.push('/');
-        }
+        };
     }
 };
 </script>
