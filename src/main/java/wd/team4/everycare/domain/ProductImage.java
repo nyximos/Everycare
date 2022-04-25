d863b018b039048cf9da89bce3e99269a50285ee
nyximos <nyximos@gmail.com>
2022-04-25 17:43:39 +0900

    제품 이미지, 제품 카테고리 테이블 추가

---
+++ src/main/java/wd/team4/everycare/domain/ProductImage.java
@@ -0,0 +1,35 @@
+package wd.team4.everycare.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "product_image")
+@SequenceGenerator(name = "product_image_seq_generator",
+        sequenceName = "product_image_seq",
+        initialValue = 1, allocationSize = 1)
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_image_seq_generator")
+    @Column(name = "product_image_id", nullable = false)
+    private Long id;
+
+    @Column(name = "product_image_name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "product_image_upload_file_name")
+    private String uploadFileName;
+
+    @Column(name = "product_image_store_file_name")
+    private String storeFileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
