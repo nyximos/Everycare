904e0c5eb06b7fb99110d91f7247e88f3bdbdd96
net6825 <net6825@naver.com>
2022-05-04 18:45:03 +0900

    temp

---
+++ src/main/java/wd/team4/everycare/repository/CertificationRepository.java
@@ -0,0 +1,14 @@
+package wd.team4.everycare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wd.team4.everycare.domain.CareSitter;
+import wd.team4.everycare.domain.Certification;
+
+import java.util.List;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+
+    List<Certification> findAllByCareSitterId(Long id);
+}
