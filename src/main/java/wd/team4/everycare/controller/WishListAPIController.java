07fe9d31f8732890ca7fda6918bbc33b98471cbe
nyximos <nyximos@gmail.com>
2022-05-16 13:17:13 +0900

    위시리스트 조회

--- src/main/java/wd/team4/everycare/controller/WishListAPIController.java
+++ src/main/java/wd/team4/everycare/controller/WishListAPIController.java
@@ -3,10 +3,7 @@ package wd.team4.everycare.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wd.team4.everycare.config.auth.PrincipalDetails;
 import wd.team4.everycare.dto.response.MyResponse;
 import wd.team4.everycare.service.WishListServiceImpl;
@@ -18,8 +15,16 @@ public class WishListAPIController {
 
     private final WishListServiceImpl wishListService;
 
+    @GetMapping("/dashboard/wishlist")
+    public ResponseEntity<MyResponse> getWishList(@AuthenticationPrincipal PrincipalDetails principalDetails){
+        ResponseEntity<MyResponse> responseEntity = wishListService.getAll(principalDetails);
+        return responseEntity;
+    }
+
     @PostMapping("/products/{id}/wish")
     public ResponseEntity<MyResponse> saveWishList(@PathVariable("id") Long id, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        wishListService.save(id, principalDetails);
+        ResponseEntity<MyResponse> responseEntity = wishListService.save(id, principalDetails);
+        return responseEntity;
     }
+
 }
--- src/main/java/wd/team4/everycare/service/WishListServiceImpl.java
+++ src/main/java/wd/team4/everycare/service/WishListServiceImpl.java
@@ -1,6 +1,7 @@
 package wd.team4.everycare.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,15 @@ import wd.team4.everycare.config.auth.PrincipalDetails;
 import wd.team4.everycare.domain.Member;
 import wd.team4.everycare.domain.Product;
 import wd.team4.everycare.domain.WishList;
+import wd.team4.everycare.dto.product.ProductListViewDTO;
+import wd.team4.everycare.dto.product.WishListDTO;
 import wd.team4.everycare.dto.response.MyResponse;
 import wd.team4.everycare.dto.response.StatusEnum;
 import wd.team4.everycare.repository.ProductRepository;
 import wd.team4.everycare.repository.WishListRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -43,5 +48,40 @@ public class WishListServiceImpl {
                 .message("성공했슴다~")
                 .build();
         return new ResponseEntity<MyResponse>(body, HttpStatus.OK);
+    }
+
+    public ResponseEntity<MyResponse> getAll(PrincipalDetails principalDetails) {
+
+        Member user = principalDetails.getUser();
+
+        List<WishList> wishList = wishListRepository.findAllByMember(user);
+
+        List<WishListDTO> wishListDTOs = new ArrayList<>();
+
+        for (WishList w : wishList) {
+            Optional<Product> product = productRepository.findById(w.getId());
+            Product productEntity = product.orElse(null);
+
+            WishListDTO dto = WishListDTO.builder()
+                    .id(productEntity.getId())
+                    .name(productEntity.getName())
+                    .price(productEntity.getPrice())
+                    .storeFileName(productEntity.getStoreFileName())
+                    .storeName(productEntity.getStore().getName())
+                    .build();
+
+            wishListDTOs.add(dto);
+        }
+
+        MyResponse<List<WishListDTO>> body = MyResponse.<List<WishListDTO>>builder()
+                .header(StatusEnum.OK)
+                .message("성공")
+                .body(wishListDTOs)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<MyResponse>(body, headers, HttpStatus.OK);
+
+
     }
 }
