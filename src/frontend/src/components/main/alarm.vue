ec91fefa8c620d8c346b68bda3997876a8103ee5
nyximos <nyximos@gmail.com>
2022-05-23 16:38:59 +0900

    Create alarm.vue

---
+++ src/frontend/src/components/main/alarm.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+            <v-list-item-content>
+                <div class="text-overline mb-4">알림</div>
+                <v-list-item-title class="text-h5 mb-1"> OOO님이 </v-list-item-title>
+                <v-list-item-subtitle>OOO님의 케어활동을 시작하였습니다!</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+            <v-btn outlined rounded text>닫기</v-btn>
+            <v-btn outlined rounded text>보기</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {};
+</script>
+
+<style></style>
