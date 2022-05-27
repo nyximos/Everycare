1ed07dc30188fba79fc17e7e4aaca500ee0d7035
nyximos <nyximos@gmail.com>
2022-05-27 11:06:47 +0900

    사용하지 않는 의존성 제거

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,7 +1,4 @@
 import Vue from 'vue';
-import vueMoment from 'vue-moment'
-import { Datetime } from 'vue-datetime'
-import 'vue-datetime/dist/vue-datetime.css'
 import App from '@/App.vue';
 import vuetify from '@/plugins/vuetify';
 import router from '@/router/index.js';
@@ -10,26 +7,20 @@ import global from '@/global/index.js';
 import Alert from '@/plugins/Alert.js';
 import axios from 'axios';
 import vueResource from 'vue-resource';
-import jquery from 'jquery'
-
+import jquery from 'jquery';
 
 global.import();
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 axios.defaults.baseURL = 'http://localhost:8086';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-
 Vue.prototype.$axios = axios;
 Vue.use(vueResource);
 Vue.use(Alert);
-Vue.use(vueMoment);
-Vue.use(Datetime);
 Vue.config.productionTip = false;
-
-
 
 new Vue({
     vuetify,
