2503c01acd3318da7b6b36a1c3fa0292ae9a07bd
seojiwon99 <83777261+seojiwon99@users.noreply.github.com>
2022-05-30 18:00:13 +0900

    케어노트

---
+++ src/frontend/src/pages/Carenote.vue
@@ -0,0 +1,18 @@
+<template>
+<v-container>
+  <Carenote />
+</v-container>
+</template>
+
+<script>
+import Carenote from '@/components/main/carenote.vue';
+export default {
+components:{
+	Carenote,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/frontend/src/pages/MemberCarenote.vue
@@ -0,0 +1,19 @@
+<template>
+      <Membernote />
+</template>
+
+<script>
+import Membernote from '@/components/main/membernote'
+
+
+export default {
+  components: {
+      Membernote
+       },
+
+};
+</script>
+
+<style>
+  
+</style>
