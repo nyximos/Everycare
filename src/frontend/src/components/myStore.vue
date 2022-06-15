d812bb8736f9ec0d8ac6410553cd085361aaa71d
박디웅 <jeun010601@naver.com>
2022-06-15 11:34:19 +0900

    내스토어 그래프

--- src/frontend/src/components/myStore.vue
+++ src/frontend/src/components/myStore.vue
@@ -1,30 +1,19 @@
 <template>
-
-<v-card height="350px">
+<div>
+  <v-row>
+    <v-col cols="2">
+ <v-card
+    height="800"
+    elevation="0"
+  >
     <v-navigation-drawer
-      absolute
       permanent
     >
-      <template v-slot:prepend>
-        <v-list-item two-line>
-          <v-list-item-avatar>
-            <img src="https://randomuser.me/api/portraits/women/81.jpg">
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title>{{id}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
+      <v-list>
         <v-list-item
           v-for="item in items"
           :key="item.title"
           link
-          @click="message(item)"
         >
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
@@ -35,8 +24,58 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Logout
+          </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
   </v-card>
+    </v-col>
+    <v-col cols="10">
+      <v-card
+       elevation="0"
+       class="mt-5"
+  >
+  <v-card-title>월별 판매량</v-card-title>
+    <v-sheet
+      color="blue"
+      max-width="calc(100% - 32px)"
+    >
+      <v-sparkline
+        :labels="labels"
+        :value="value"
+        color="white"
+        line-width="2"
+        padding="16"
+      ></v-sparkline>
+    </v-sheet>
+
+    <v-card-text class="pt-0">
+      <div class="text-h6 font-weight-light mb-2">
+        User Registrations
+      </div>
+      <div class="subheading font-weight-light grey--text">
+        Last Campaign Performance
+      </div>
+      <v-divider class="my-2"></v-divider>
+      <v-icon
+        class="mr-2"
+        small
+      >
+        mdi-clock
+      </v-icon>
+      <span class="text-caption grey--text font-weight-light">last registration 26 minutes ago</span>
+    </v-card-text>
+  </v-card>
+    </v-col>
+
+      
+  </v-row>
+</div>
 </template>
 
 <script>
@@ -61,6 +100,34 @@ data(){
           { title: '상품 수정', icon: 'mdi-account', name:'prodEdit'},
           { title: '스토어 수정', icon: 'mdi-account-group-outline', name:'storeEdit' },
         ],
+ labels: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12'
+      ],
+      value: [
+        200,
+        675,
+        410,
+        390,
+        310,
+        460,
+        250,
+        240,
+        100,
+        100,
+        150,
+        100
+      ],
       }
     },
     methods:{
