13f35469e1d6ee43c2f022a6a2567c97848df2db
sungjae4 <99310245+sungjae4@users.noreply.github.com>
2022-06-19 02:21:02 +0900

    commit

--- src/frontend/src/components/jobapplication.vue
+++ src/frontend/src/components/jobapplication.vue
@@ -1,6 +1,57 @@
 <template>
 <div>
-  <v-card v-for="(p,index) in profiles" :key="index"
+  <div class="content">
+	<div class="match">
+<link rel="stylesheet" href="/css/cupertino.datepicker.css?v=202103040001">
+
+
+	<div class="match_div"> 
+		<h3 class="tit">채용내역</h3>
+
+		<div class="con_blank01"></div>
+		<div class="con_bar01"></div>
+		<div class="con02">
+		
+			<ul class="tab01">
+				<a href="javascript:tab_list(1);"><li class="menu01">받은 내역</li></a>
+				<a href="javascript:tab_list(2);"><li class="menu02 on">신청 내역</li></a>
+			</ul>
+		</div>
+		<div class="con_bar02"></div>
+		<div class="con03 tab1" id="div01" style="display: none;">
+			<ul class="list01">
+												<li class="con0302">
+				현재 받은 내역이 없습니다.
+				</li>
+							</ul>
+		</div>
+    
+		<div class="con03 tab2" id="div02" style="display: block;"  v-for="(p,index) in profiles" :key="index"
+     >
+			<ul class="list01">
+								<li class="con0301">
+					<a href="/caresitters/${p.careSitterDTO.id}" target="_BLANK"><span class="more01">상세보기</span></a>
+					<!-- <a href="/resume/view.php?encrypt_id=bldQVktKbmhtanRQM1F4K3ErR1NOUT09" target="_BLANK"><span class="date01">2022-06-18 19:23:09</span> -->
+					<span class="name01">{{p.memberDTO.name}}</span>
+					<span class="sex01">{{p.memberDTO.gender}} <span class="bar01">|</span> {{p.memberDTO.birth}}</span>
+					<span class="pay01">* 희망급여 <span>시급 : <span style="color: #3c3c3c">{{p.pay}}원</span></span></span>
+          					<span class="pay01">* 케어날짜 <span>시작일 : <span style="color: #3c3c3c">{{p.startDate}}</span></span> <span class="bar01">|</span> <span>종료일 : {{p.endDate}}</span></span>
+					
+					<span class="subject01">{{p.careSitterDTO.introduction}}</span>
+					<span class="con_bar04"></span>
+										<span class="btn01">
+					<input type="button" value="승낙 및 결제" class="btn0103" @click="payment(p)">
+          <!-- <input type="button" value="승낙 및 결제" class="btn0103" onclick="javascript:alert('신청해주신 내역이 아직 처리되지 않았습니다.');"></input> -->
+					</span>
+									</li>
+											</ul>
+		</div>
+
+		<div class="layer01" id="layer_match_list">
+		</div>
+	</div>	</div>
+</div>
+  <!-- <v-card v-for="(p,index) in profiles" :key="index"
     class="mx-auto" 
     max-width="344"
     outlined
@@ -34,7 +85,7 @@
       </v-btn>
     </v-card-actions>
   
-  </v-card>
+  </v-card> -->
   
 </div>
 
@@ -111,9 +162,13 @@ export default {
 //     }
 // }
 }
+
 </script>
 
-<style>
+<style scoped>
+li{
+  list-style: none;
+}
 .mx-auto{
   display: inline-block;
     text-align: center;
@@ -123,4 +178,118 @@ export default {
     font-weight: bold;
     color: #222;
 }
+.content { width:100%; position:relative; }
+.content .match .con_bar02 { float:left; }
+.content .match { width:100%; max-width:670px; min-height:800px; background-color:#FFFFFF; position:relative; margin:0 auto; border-left:1px solid #DEDEDE; border-right:1px solid #DEDEDE; }
+.content .match .match_div { width:100%; max-width:670px; padding:30px 0 0 0; box-sizing:border-box; position:relative; }
+.content .match .match_div h3.tit { width:100%; padding:0 15px 0 15px; box-sizing:border-box; position:relative; font-size:1.3em; font-weight:500; color:#333333; }
+
+.content .match .match_div .con01 { width:100%; position:relative; margin:0 auto; padding:15px 15px 15px 15px; box-sizing:border-box; border-top:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; overflow:hidden; }
+.content .match .match_div .con01 .profile { width:75px; margin:0 15px 0 0; float:left; border-radius:100px; position:relative; display:inline-block; }
+.content .match .match_div .con01 .profile img { width:100%; position:relative; display:inline-block; }
+.content .match .match_div .con01 .name { width:calc(100% - 90px); position:relative; display:inline-block; text-decoration:underline;}
+.content .match .match_div .con01 .phone { width:calc(100% - 90px); position:relative; display:inline-block; padding:10px 0 0 0; letter-spacing:0; }
+.content .match .match_div .con01 .pay { width:calc(100% - 90px);  position:relative; display:inline-block; padding:5px 0 0 0 }
+.content .match .match_div .con01 .modify { display:inline-block; width:auto; background-color:#FFFFFF; border:0; border-radius:25px; color:#2c786c; font-size:0.8em; padding:5px; margin:0 0 0 10px;}
+.content .match .match_div .con01 .paysubmit { position:absolute; display:inline-block; padding:10px; background-color:#f8b400; border:0; border-radius:25px; color:#FFFFFF; right:15px; top:15px; font-size:0.9em; }
+
+.content .match .match_div .con02 { width:100%; position:relative; margin:0 auto; padding:0 0 0 0; box-sizing:border-box; overflow:hidden; }
+.content .match .match_div .con02 .tab01 { width:100%; position:relative; }
+.content .match .match_div .con02 .tab01 .menu01 { width:50%; padding:20px 0 20px 0; box-sizing:border-box; position:relative; border-bottom:1px solid #EFEFEF; float:left; text-align:center; background-color:#2c786c2e; color:#2c786ca8 }
+.content .match .match_div .con02 .tab01 .menu02 { width:50%; padding:20px 0 20px 0; box-sizing:border-box; position:relative; border-bottom:1px solid #EFEFEF; float:left; text-align:center; background-color:#2c786c2e; color:#2c786ca8 }
+.content .match .match_div .con02 .tab01 .on { color:#FFFFFF; background-color:#2c786c; }
+
+
+.content .match .match_div .con03 { width:100%; position:relative; margin:0 auto; padding:15px; box-sizing:border-box; overflow:hidden; display:none; }
+.content .match .match_div .con03 .list01 { width:100%; border:1px solid #DEDEDE; border-radius:10px; position:relative; margin:0 auto; padding:10px; box-sizing:border-box; overflow:hidden; }
+.content .match .match_div .con03 .con0301 { width:100%; border-bottom:1px solid #DEDEDE; position:relative; margin:10px auto 10px auto; padding:10px; box-sizing:border-box; overflow:hidden; line-height:1.2em; }
+.content .match .match_div .con03 .con0302 { width:100%; position:relative; margin:10px auto 10px auto; padding:30px 10px 30px 10px; box-sizing:border-box; overflow:hidden; line-height:1.2em; text-align:center; }
+.content .match .match_div .con03 .con0301 .more01 { position:absolute; float:right; right:10px; display:inline-block; margin:0 0 10px 0; font-size:0.9em; color:#FFFFFF; background-color:#2c786c; border:1px solid #2c786c; border-radius:30px; padding:3px 10px 3px 10px; }
+.content .match .match_div .con03 .con0301 .date01 { display:inline-block; width:100%; margin:0 0 10px 0; font-size:0.9em; color:#9A9A9A }
+.content .match .match_div .con03 .con0301 .date02 { display:inline-block; width:100%; margin:0 0 10px 0; font-size:0.9em; color:#373737; text-align:center; }
+.content .match .match_div .con03 .con0301 .date02 .head01 { display:inline-block; color:#A9A9A9; margin:0 10px 0 0; }
+.content .match .match_div .con03 .con0301 .date02 .cont01 { display:inline-block; color:#2c786c; font-size:1em }
+.content .match .match_div .con03 .con0301 .date02 .icon01 { display:inline-block; background-color:#2c786c; font-size:0.9em; padding:0 5px; color:#FFFFFF; border-radius:3px; margin:0 5px 0 0; font-weight:400 }
+.content .match .match_div .con03 .con0301 .date02 .icon02 { display:inline-block; background-color:#f8b400; font-size:0.9em; padding:0 5px; color:#FFFFFF; border-radius:3px; margin:0 5px 0 0; font-weight:400 }
+.content .match .match_div .con03 .con0301 .date02 .icon03 { display:inline-block; background-color:#F4F4F4; font-size:0.9em; padding:0 5px; color:#9A9A9A; border-radius:3px; margin:0 5px 0 0; font-weight:400 }
+.content .match .match_div .con03 .con0301 .name01 { display:inline-block; margin:0 5px 5px 0; }
+.content .match .match_div .con03 .con0301 .sex01 { display:inline-block; margin:0 5px 0 0; font-size:0.9em; color:#797979 }
+.content .match .match_div .con03 .con0301 .area01 { display:inline-block; margin:0 5px 0 0; font-size:0.9em; color:#797979 }
+.content .match .match_div .con03 .con0301 .pay01 { display:inline-block; width:100%; margin:0 0 0 0; font-size:0.8em; color:#797979 }
+.content .match .match_div .con03 .con0301 .insurance01 { display:inline-block; width:100%; margin:0 0 5px 0; font-size:0.8em; color:#9A9A9A }
+.content .match .match_div .con03 .con0301 .service_date01 { display:inline-block; width:100%; margin:0 0 5px 0; font-size:0.8em; color:#797979 }
+.content .match .match_div .con03 .con0301 .subject01 { display:inline-block; width:100%; margin:0 0 0 0; }
+.content .match .match_div .con03 .con0301 .bar01 { display:inline-block; margin:0 0 0 0; font-size:0.7em; color:#DEDEDE }
+.content .match .match_div .con03 .con0301 .btn01 { width:100%; display:inline-block; text-align:center; }
+.content .match .match_div .con03 .con0301 .btn01 .btn0101 { border:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:calc(50% - 8px); margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .con03 .con0301 .btn01 .btn0102 { border:1px solid #DEDEDE; background-color:#F4F4F4; color:#797979; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:calc(50% - 8px); margin:0 0px 0 6px;  text-align:center;}
+.content .match .match_div .con03 .con0301 .btn01 .btn0103 { border:1px solid #DEDEDE; background-color:#F4F4F4; color:#797979; border-radius:5px; padding:10px; display:inline-block; width:calc(50% - 10px); margin:0 3px 0 3px;  text-align:center;}
+.content .match .match_div .con03 .con0301 .btn01 .btn0104 { border:1px solid #f8b400; background-color:#f8b400; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:calc(50% - 8px); margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .con03 .con0301 .btn01 .btn0104.on { border:1px solid #f8b400; background-color:#f8b400; color:#000000; font-weight:bold; }
+
+.content .match .match_div .con03 .con0301 .btn01 .btn0105 { border:1px solid #f8b400; background-color:#f8b400; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:calc(50% - 8px); margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .con03 .con0301 .btn01 .btn0106 { border:1px solid #DEDEDE; background-color:#F4F4F4; color:#373737; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:calc(50% - 8px); margin:0 6px 0 0; text-align:center; font-weight:600; }
+
+.content .match .match_div .layer01 { width:100%; position:relative; display:none; }
+.content .match .match_div .layer01 .layer0101 { position:fixed; border-radius:10px; max-width:640px; width:90%; left:0; right:0; margin:0 auto; top:calc( 100% / 2 - 250px ); border:1px solid #DEDEDE; background-color:#FFFFFF; z-index:22; padding:20px; box-sizing:border-box; box-shadow:3px 3px 10px 2px #797979; text-align:center; }
+.content .match .match_div .layer01 .layer0101 .text01 { display:inline-block; width:100%; text-align:center; padding:20px 0 10px 0; box-sizing:border-box;}
+.content .match .match_div .layer01 .layer0101 .btn01 { width:100%; display:inline-block; text-align:center; padding:30px 0 50px 0;}
+.content .match .match_div .layer01 .layer0101 .btn01 .btn0101 { border:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:30%; margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .layer01 .layer0101 .btn01 .btn0102 { border:1px solid #f8b400; background-color:#f8b400; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:30%; margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .layer01 .layer0101 .btn01 .btn0103 { border:1px solid #DEDEDE; background-color:#F4F4F4; color:#9A9A9A; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:30%; margin:0 6px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .layer01 .layer0101 .text02 { display:none; width:100%; text-align:center; padding:40px 0 20px 0; box-sizing:border-box; border-radius:10px; background-color:#F4F4F4; }
+.content .match .match_div .layer01 .layer0101 .text02 .text0201 { line-height:1.3em; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 { width:100%; display:inline-block; text-align:center; padding:30px 0 20px 0;}
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .btn0201 { border:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:45%; margin:0 3px 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .btn0202 { border:1px solid #DEDEDE; background-color:#DEDEDE; color:#9A9A9A; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:45%; margin:0 0 0 3px; text-align:center; font-weight:600;}
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .text0202 .date01 { position:relative; width:100%; text-align:center; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .btn0203 { border:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:90%; margin:10px 0 0 0; text-align:center; font-weight:600; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .btn0204 { border:1px solid #DEDEDE; background-color:#DEDEDE; color:#9A9A9A; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:90%; margin:15px 0 0 0; text-align:center; font-weight:600;}
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .bar02 { display:inline-block; width:80%; margin:0 auto; border-top:1px solid #DEDEDE}
+
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .text0203 { display:inline-block; position:relative; width:100%; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .text0203 .select01 { width:90%; box-sizing:border-box; height:42px; font-size:0.8em; text-align:center; letter-spacing:0; border-radius:5px; border:1px solid #2c786c; margin:0 0 0 0; text-indent:10px;  }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .text0203 .textarea01 { display:inline-block; width:90%; box-sizing:border-box; height:150px; font-size:0.9em; text-align:left; letter-spacing:0; border-radius:5px; border:1px solid #2c786c; margin:10px 0 0 0; text-indent:0; padding:10px; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .text0203 .textarea01::placeholder { line-height:1.5em; color:#9A9A9A }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .star01 { text-align:center; color:#2c786c; display:inline-block; padding:0px 0 10px 0; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .star01 .icon_star { color:#2c786c; margin:0 5px 0 5px; font-size:1.8em; letter-spacing:0; content:'☆' }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .star01 .icon_star:after { content:'☆' }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .star01 .icon_star.on { color:#f8b400; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .star01 .icon_star.on:after { content:'★'  }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .btn0205 { border:1px solid #2c786c; background-color:#2c786c; color:#FFFFFF; border-radius:5px; padding:10px; display:inline-block; box-sizing:border-box; width:90%; margin:10px 0 0 0; text-align:center; font-weight:600; }
+
+
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .sample01 { display:inline-block; position:relative; width:100%; margin:7px 0 0 0; }
+.content .match .match_div .layer01 .layer0101 .text02 .btn02 .sample01 .select01 { width:90%; box-sizing:border-box; height:42px; font-size:0.8em; text-align:left; letter-spacing:0; border-radius:5px; border:1px solid #2c786c; margin:0 0 0 0; text-indent:5px;  }
+#ui-datepicker-div { display:none; top:180px !important; z-index:33 !important; position:fixed !important; margin:0 auto 0 auto; left:0 !important; right:0 !important; width:90%; max-width:320px; box-sizing:border-box; }
+.ui-datepicker-trigger { width:41px; height:40px; border:1px solid #2c786c; border-radius:5px; vertical-align:top; }
+#input_interview_date { width:calc(90% - 53px); height:42px; font-size:1.1em; text-align:center; letter-spacing:0; border-radius:5px; box-sizing:border-box; border:1px solid #2c786c; margin:0 5px 0 0; text-indent:10px; }
+html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;outline:0;font-size:100%;}
+body{line-height:1; font-size:1em; font-family:"Noto Sans KR", sans-serif; color:#3c3c3c; letter-spacing:-0.05em}
+html,body{background:#FFFFFF}
+article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}
+nav ul{list-style:none}
+blockquote,q{quotes:none}
+blockquote:before,blockquote:after,q:before,q:after{content:none}
+a{margin:0;padding:0;font-size:100%;vertical-align:baseline;background:transparent;text-decoration:none;color:#666;}
+a:link,a:visited {color:#000;text-decoration:none;}
+a:hover,a:focus,a:active{color:#000;text-decoration:none;}
+ins{background-color:#ff9;color:#000;text-decoration:none}
+mark{background-color:#ff9;color:#000;font-style:italic;font-weight:bold}
+del{text-decoration:line-through}
+abbr[title],dfn[title]{border-bottom:1px dotted;cursor:help}
+table{border-collapse:collapse;border-spacing:0}
+hr{display:block;height:1px;border:0;border-top:1px solid #ccc;margin:1em 0;padding:0}
+input,select{vertical-align:middle; background-color:#FFFFFF }
+
+input:focus, textarea:focus, select:focus, option:focus {
+    outline:none !important;
+}
+/*�߰�*/
+table{width:100%}
+ol, ul {list-style:none}
+address, caption, em, cite {font-weight:normal;font-style:normal}
+fieldset, img, abbr, acronym {border:none}
+img {border:0;vertical-align:baseline}
+button {overflow:visible;margin:0;padding:0;border:0 none;background:transparent;cursor:pointer}
 </style>
--- src/frontend/src/components/main/mypage.vue
+++ src/frontend/src/components/main/mypage.vue
@@ -1,5 +1,132 @@
 <template>
-  <div>
+<div class="wrap">
+  <div class="greenContainer">
+    <div>
+      <div class="grade">GOLD</div>
+      <div class="name">Jaden</div>
+    </div>    
+    <div class="modify">i</div>    
+  </div>
+  <div class="summaryContainer">
+    <div class="item">
+        <div class="number">354</div>
+        <div>단골상점</div>
+      </div>
+      <div class="item">
+        <div class="number">354</div>
+        <div>상품후기</div>
+      </div>
+      <div class="item">
+        <div class="number">354</div>
+        <div>적립금(BLCT)</div>
+      </div>
+  </div>  
+   
+  <div class="listContainer">
+    <div v-if="this.$store.state.userStore.careSitterId  !==null ||  this.$store.state.careprofileStore.activityTime !== ''" >
+    <a href="#" class="item" >
+         
+        <div class="icon" >ii</div>
+        <router-link :to="{name:'certification',params:{caresitterId:this.id}}"><div class="text">자격증 등록<span class="circle"></span></div></router-link>
+        <div class="right"> > </div>
+        
+    </a>
+    
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+        <router-link :to="{name:'mypage_certi' ,params:{caresitterId:this.id}}"><div class="text">케어 자격증 조회</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+        <router-link :to="{name:'mypage_profile' ,params:{caresitterId:this.id}}"> <div class="text">프로필 조회</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+        <router-link :to="{name:'careprofilecreate'}"><div class="text">케어 프로필 생성</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    </div>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+        <div class="text">케어 대상인 등록</div>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'mypage_joblist'}"><div class="text">내 구인공고 관리</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'memberSchedule', params:{caresitterId:this.id}}"><div class="text">채용내역</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    
+  </div>
+  <div class="listContainer">
+    <!-- <a href="#" class="item">
+        <div class="icon">ii</div>
+        <div class="text">
+          <span>내지갑</span>
+          <span class="smallLight">
+            <span>|</span>
+            <span>보유 적립금</span>
+          </span>          
+        </div>                
+        <div class="right">
+          <span class="blct">175 BLCT</span>
+          > </div>
+    </a> -->
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'memberSchedule', params:{caresitterId:this.id}}"><div class="text">스케줄</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'caresitter_contract', params:{caresitterId:this.id}}"><div class="text">계약서 조회</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'membernote'}"><div class="text">케어노트</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+         <router-link :to="{name:'complete_contract'}"><div class="text">완료된 계약서</div></router-link>
+        <div class="right"> > </div>
+    </a>
+    <!-- <a href="#" cl
+    ass="item">
+        <div class="icon">ii</div>
+        <div class="text">알림</div>
+        <div class="right"> > </div>
+    </a>
+    <a href="#" class="item">
+        <div class="icon">ii</div>
+        <div class="text">설정</div>
+        <div class="right"> > </div>
+    </a>    -->
+  </div>
+  <div class="infoContainer">
+    <a href="#" class="item">
+      <div>icon</div>
+      <div>공지사항</div>
+    </a>    
+    <a href="#" class="item">
+      <div>icos</div>
+      <div>이용안내</div>
+    </a>    
+    <a href="#" class="item">
+      <div>icon</div>
+      <div>고객센터</div>
+    </a>
+  </div>
+</div>
+  <!-- <div>
       <h2>마이페이지</h2>
       <div v-if="this.$store.state.userStore.careSitterId  !==null ||  this.$store.state.careprofileStore.activityTime !== ''" >
         <router-link :to="{name:'certification',params:{caresitterId:this.id}}"><p>자격증 등록</p></router-link>
@@ -16,7 +143,7 @@
         <router-link :to="{name:'membernote'}"><p>케어노트(멤버용)</p></router-link>
         <router-link :to="{name:'complete_contract'}"><p>완료된 계약서(케어시터)</p></router-link>
         <router-link :to="{name:'complete_target'}"><p>완료된 계약서(회원)</p></router-link>
-    </div>
+    </div> -->
   
 </template>
 
@@ -35,5 +162,194 @@ export default {
 </script>
 
 <style>
+body{
+  padding: 0;
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
 
+/* alert badge */
+.circle{
+  display: inline-block;
+  width: 5px;
+  height: 5px;
+  border-radius: 2.5px;
+  background-color: #ff0000;
+  position: absolute;
+  top: -5px;
+  left: 110%;
+}
+
+/* 녹색 텍스트 */
+.green{
+  color: #24855b;
+}
+
+.wrap{
+  background-color: #F8F8F8;  
+}
+/* 녹색배경 */
+.greenContainer{  
+  height: 132px;
+  background-color: #24855b;    
+  
+  display: flex;
+  align-items: flex-end;
+  padding: 16px;
+}
+
+.greenContainer .name{
+   font-size: 20px;
+  font-weight: bold;
+  color: #ffffff;
+} 
+.greenContainer .modify{
+  margin-left: auto;
+}
+
+/* 단골상점 , 상품후기 , 적립금 박스 */
+.summaryContainer{
+  background-color: white;  
+  display: flex;  
+  padding: 21px 16px;  
+  height: 90px;
+  margin-bottom: 10px;
+}
+/* 단골상점 , 상품후기 , 적립금 */
+.summaryContainer .item{
+  flex-grow: 1
+}
+/* 녹색 숫자 */
+.summaryContainer .number{
+  font-size: 19px;
+  font-weight: bold;
+  color: #24855b;
+}
+/* 텍스트 */
+.summaryContainer .item > div:nth-child(2){
+  font-size: 13px;
+}
+
+/* ================== 주문/배송조회 박스 시작 ==================== */
+.shippingStatusContainer{
+  padding: 21px 16px;
+  background-color: white;
+  margin-bottom: 10px;
+}
+
+/* 주문/배송조회 타이틀 */
+.shippingStatusContainer .title{
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 15px;
+}
+
+/* 장바구니 결제완료 배송중 구매확정 [로우] */
+.shippingStatusContainer .status{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 21px;
+}
+/* 장바구니 결제완료 배송중 구매확정 [아이템]  */
+.shippingStatusContainer .item{
+  display: flex;
+}
+
+.shippingStatusContainer .number{
+  font-size: 31px;
+  font-weight: 500;
+  text-align: center;
+}
+.shippingStatusContainer .text{
+  font-size: 12px;
+  font-weight: normal;
+  color: #c2c2c2;
+  text-align: center;
+}
+.shippingStatusContainer .icon{
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  width: 16px;
+  height: 16px;
+}
+
+
+/*=================== 주문목록 ~ 찜한상품 리스트 ==================*/
+.listContainer{  
+  padding: 0;
+  background-color: #ffffff;
+  margin-bottom: 10px;
+}
+.listContainer .item{  
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  color: black;
+  text-decoration: none;  
+  height: 56px;
+  box-sizing: border-box;
+}
+.listContainer .icon{  
+  margin-right: 14px;
+}
+.listContainer .text{
+  font-size: 16px;
+  position: relative;
+}
+.listContainer .right{
+  margin-left: auto;
+}
+
+
+/*=================== 내지갑의 보유 적립금 들어가는 부분 ================*/
+.listContainer .smallLight{
+  font-size: 14px;
+  color: #c2c2c2;  
+}
+.listContainer .smallLight > span{
+  margin-left: 10px;
+}
+
+.listContainer .right .blct{
+  font-size: 14px;
+  font-weight: bold;
+  margin-right: 5px;
+}
+
+
+
+/* 공지사항 이용안내 고객센터 */
+.infoContainer{
+  background-color: white; 
+  display: flex;
+  height: 100px;
+  margin-bottom: 10px;    
+}
+
+.infoContainer .item{
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 13px;
+  text-decoration: none;
+  color: black;
+}
+.infoContainer .item > div:first-child{
+  margin-bottom: 2px;
+}
+
+
+
+/*  */
+.listContainer .item:hover{
+/*   background-color: #f8f8f8; */
+}
+.infoContainer .item:hover{
+/*   background-color: #f8f8f8; */
+}
 </style>
