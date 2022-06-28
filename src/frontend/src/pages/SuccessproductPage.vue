cfe95ff3b3fb1e6b7a0d020f87ae40d7550794f8
박디웅 <jeun010601@naver.com>
2022-06-28 15:34:04 +0900

    결제완료페이지수정

--- src/frontend/src/pages/SuccessproductPage.vue
+++ src/frontend/src/pages/SuccessproductPage.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>결제 완료</h1>
+<div class="text-center">
+  <h1><v-icon color="green" class="mt-10" large> mdi-checkbox-marked-circle</v-icon></h1>
+<h1 class="fw-bold mt-10" >결제가 완료되었습니다</h1>
+  <v-btn outlined @click="main" class="mt-10" color="grey">메인으로</v-btn>
+</div>
+  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods:{
+    main(){
+      this.$router.push({
+      path: '/',
+      })
+    }
+  }
+}
+</script>
