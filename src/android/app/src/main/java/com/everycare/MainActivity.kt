a6de2c9eaa5713edfc681fd2fa7a89e498b03674
nyximos <76250754+nyximos@users.noreply.github.com>
2022-06-29 14:47:35 +0900

    메인 로그인 화면

--- src/android/app/src/main/java/com/everycare/MainActivity.kt
+++ src/android/app/src/main/java/com/everycare/MainActivity.kt
@@ -2,8 +2,14 @@ package com.everycare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.everycare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
